3e552b00a93ffae4760084ef197ef97e4e6958a8
repoman322 <kegger@users.noreply.github.com>
2021-05-17 19:37:22 +0000

    MAnipulating lists Lab 10

---
+++ listmethods/listmeth02.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+proto = ["ssh", "http", "https"]
+protoa = ["ssh", "http", "https"]
+print(proto)
+proto.append("dns") # this line will add "dns" to the end of our list
+protoa.append("dns") # this line will add "dns" to the end of our list
+print(proto)
+proto2 = [ 22, 80, 443, 53 ] # a list of common ports
+proto.extend(proto2) # pass proto2 as an argument to the extend method
+print(proto)
+protoa.append(proto2) # pass proto2 as an argument to the append method
+print(protoa)
+
+# add item to the list
+protoa.append("ssh")
+# count the number of times ssh is on the list
+print(protoa.count("ssh"))
+
+# try revrese
+#proto.reverse()
+print(protoa)
