15aeb63e83388001f1d834b2d694e49ed6bef358
repoman322 <kegger@users.noreply.github.com>
2021-05-17 17:58:09 +0000

    learning about input and Lunchtime2

--- lab_input/iptaker02.py
+++ lab_input/iptaker02.py
@@ -5,5 +5,7 @@ user_input = input("Please enter an IPv4 IP address:")
 # print("You told me the IPv4 address is:" + user_input)
 
 ## print() can be given a series of objects separated by a comma
-print("You told me the IPv4 address is:", user_input)
+print("You told me the IPv4 address is: ", user_input)
+vendor = input("Please enter the vendor name for this gear: ")
+print("Vendor entered was",vendor," Not bad..")
 
