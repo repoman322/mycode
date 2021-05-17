fd2a0d86739cd45177b84e7a3330ec8061a3ad89
repoman322 <kegger@users.noreply.github.com>
2021-05-17 20:50:19 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# create a list called list1
+animals = ["Fox", "Fly", "Ant", "Bee", "Cod", "Cat", "Dog", "Yak", "Cow", "Hen", "Koi", "Hog", "Jay", "Kit"]
+# display animals
+print(animals[0],animals[1],animals[2],animals[3],animals[4],animals[5],animals[6],animals[7],animals[8],animals[9],animals[10],animals[11],animals[12],animals[13])
+print(animals[0:14])
+for each in animals:
+    print(each, end=" ")
+print()
