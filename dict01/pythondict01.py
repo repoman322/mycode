0e5a4aa7c17c2bff9054cd46fc9c16066e66a432
repoman322 <kegger@users.noreply.github.com>
2021-05-18 13:56:20 +0000

    learning about dictionaries

--- dict01/pythondict01.py
+++ dict01/pythondict01.py
@@ -10,13 +10,13 @@ print( switch["ip"] )
 ## request a 'fake' key
 #print( switch["lynx"] )  
 ## request a 'fake' key with .get() method
-print( "First test - .get()" )
+print( "First test - .get('lynx')" )
 print( switch.get("lynx") )
 
-print( "Second test - .get()" )
+print( "Second test - .get('lynx')" )
 print( switch.get("lynx", "THE KEY IS IN ANOTHER CASTLE!") )
 
-print( "Third test - .get()" )
+print( "Third test - .get('version')" )
 print( switch.get("version") )
 
 print( "Fourth test - .keys()" )
@@ -25,17 +25,17 @@ print( switch.keys() )
 print( "Fifth test - .values()" )
 print(switch.values())
 
-print( "Sixth test - .pop()" )
+print( "Sixth test - .pop('version')" )
 switch.pop("version")    # removes this key (and value) pair
 print( switch.keys() )   # notice the value of version is gone
 print( switch.values() ) # notice the value 1.2
 
-print( "Seventh test - ADD a new value" )
+print( "Seventh test - ADD a new key/value:  adminlogin : karl08" )
 switch["adminlogin"] = "karl08"
 print( switch.keys() )
 print( switch.values() )
 
-print( "Eighth test - ADD a new value" )
+print( "Eighth test - ADD a new key/value:  password : ****" )
 switch["password"] = "qwerty"
 print( switch.keys() )
 print( switch.values() )
